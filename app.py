7c57018cea757b287aa2ed75cac964f31af4206f
19ravisha <19ravisha@gmail.com>
2026-02-06 18:09:08 +0530

    Initial Rag commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from openai import OpenAI
+import faiss, pickle
+from sentence_transformers import SentenceTransformer
+
+app = FastAPI()
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+index = faiss.read_index("faiss.index")
+
+with open("chunks.pkl", "rb") as f:
+    chunks = pickle.load(f)
+
+import os
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+@app.get("/query")
+def query_rag(q: str):
+    query_vec = model.encode([q])
+    D, I = index.search(query_vec, 3)
+
+    context = "\n".join([chunks[i]["text"] for i in I[0]])
+
+    prompt = f"Answer only using context:\n{context}\n\nQuestion:{q}"
+
+    response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[{"role":"user","content":prompt}],
+        temperature=0.2
+    )
+
+    return {"answer": response.choices[0].message.content}
